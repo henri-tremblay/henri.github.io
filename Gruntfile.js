85d0238ef0c3c8afda4ba9e4143dac5538415f6e
Henri Tremblay <Henri.Tremblay@pj.ca>
2016-03-29 01:50:31 -0400

    Add html validation

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = function (grunt) {
+  grunt.loadNpmTasks('grunt-w3c-html-validation                                                                                                                               ');
+
+  grunt.initConfig({
+    validation: {
+      options: {
+        reset: grunt.option('reset') || false,
+        stoponerror: false,
+        relaxerror: ['Bad value X-UA-Compatible for attribute http-equiv on element meta.'] //ignores these errors
+      },
+      files: {
+        src: ['_site/**/*.html']
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['validation']);
+};
